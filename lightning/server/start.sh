a0bc327b83921fb202db61976fe2b90ee59f47af
Binzi Cao <Binzi.Cao@zetaris.com>
2024-08-22 10:49:38 +1000

    Add user jdbc support

--- lightning/server/start.sh
+++ lightning/server/start.sh
@@ -3,6 +3,8 @@
 chmod -R 777 $filesystem_staging
 chown -R zetaris:zetaris $filesystem_staging
 rm -rf /home/zetaris/lightning/lib/com.nvidia.rapids*.jar
+mkdir $LIGHTNING_HOME/logs
+cp /home/zetaris/data/lib/jdbc/*  /home/zetaris/lightning/jdbc-driver/
 bash $LIGHTNING_HOME/bin/stop-lightning-server.sh
 bash $LIGHTNING_HOME/bin/start-lightning-server.sh
 tail -f /opt/spark/logs/*.out
